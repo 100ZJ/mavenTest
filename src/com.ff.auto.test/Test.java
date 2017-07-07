d765669091346fc1b22946ce4f445dc47c83660f
Jing Zhang (张京30) <zhangjing30@le.com>
2017-07-07 17:02:09 +0800

    Initial commit

---
+++ src/com.ff.auto.test/Test.java
@@ -0,0 +1,14 @@
+package src.com.ff.auto.test;
+
+/**
+ * Created by zhangjing30 on 2017/7/7.
+ */
+public class Test {
+
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
